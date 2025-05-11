03c785905b1eb91fdd12f75687da0234c9a79085
TunmiDev <133062206+TunmiDev@users.noreply.github.com>
2025-05-11 03:33:00 +0100

    removed console.log

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 function displayPoem(response) {
-  console.log("poem generated");
-
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -23,10 +21,6 @@ function generatePoem(event) {
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
   poemElement.innerHTML = `<div class = "generating">⌛Generating a poem about ${promptInput.value} </div>`;
-
-  console.log("Generating poem");
-  console.log(`prompt : ${prompt}`);
-  console.log(`context : ${context}`);
 
   axios.get(apiURL).then(displayPoem);
 }
